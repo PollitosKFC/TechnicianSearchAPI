043c8569d836cac3c57428a44ce05b1135776b44
ShoLee01 <miguelalfaro2017@gmail.com>
2022-05-03 20:11:32 -0500

    feat: creating proyect

---
+++ src/main/java/pe/edu/upc/techniciansearchapi/EasyJobs/EasyJobsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.techniciansearchapi.EasyJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyJobsApplication.class, args);
+	}
+
+}
